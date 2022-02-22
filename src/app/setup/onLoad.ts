bda54d4e41c57fd182e4005e1bd03a1f9b49e101
dtchitt <30878518+dtchitt@users.noreply.github.com>
2022-02-21 21:56:21 -0800

    added trees and updated player-type

--- src/app/setup/onLoad.ts
+++ src/app/setup/onLoad.ts
@@ -1,7 +1,9 @@
 import CameraControls from "app/camera-controls";
+import { GamePlayer, PlayerNames } from "app/player/player-type";
 import { UserInterface } from "app/user-interface-type";
 import { Util } from "libs/translators";
 import { HexColors } from "resources/hexColors";
+import { Players } from "w3ts/globals";
 import { GameStatus } from "./game-status";
 
 /**
@@ -11,4 +13,26 @@ export function onLoad() {
     print(`${Util.RandomEnumKey(HexColors)}Game type is:|r ${Util.RandomEnumKey(HexColors)}${GameStatus.getInstance().toString()}|r`);
     UserInterface.onLoad();
     CameraControls.getInstance();
+
+    Players.forEach(player => {
+        player.name = PlayerNames.shift();
+
+        if (player.slotState == PLAYER_SLOT_STATE_PLAYING) {
+            if (player.id >= 25) return;
+
+            GamePlayer.fromID.set(player.id, new GamePlayer(player.handle));
+            //TODO: Add to ping force
+            //TODO: update status
+        }
+    })
+
+    //TODO
+    //transport events
+    //change colors
+    //init kd maps
+    //change name after colors
+    //promode
+    //trees
+    //city allocation
+    
 }
