20fcd851cf83488c4c7dc704464155311fa5398a
dtchitt <30878518+dtchitt@users.noreply.github.com>
2022-06-13 12:47:30 -0700

    w3mmd init handling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Game } from "app/game/game-type";
 import { addScriptHook, W3TS_HOOK } from "w3ts";
+import { stopInit } from "w3ts-w3mmd";
 
 function tsMain() {
 	try {
@@ -14,6 +15,7 @@ addScriptHook(W3TS_HOOK.MAIN_AFTER, tsMain);
 
 function fixMapName() {
 	SetMapName("Risk Europe");
+	stopInit();
 }
 
 addScriptHook(W3TS_HOOK.CONFIG_AFTER, fixMapName);
