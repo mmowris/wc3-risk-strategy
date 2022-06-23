f12e38bbf24b95fd794411ce2de4beae6199518f
dtchitt <30878518+dtchitt@users.noreply.github.com>
2022-06-22 19:57:48 -0700

    rank tracking update

--- src/app/game/game-ranking-helper-type.ts
+++ src/app/game/game-ranking-helper-type.ts
@@ -82,103 +82,108 @@ export class GameRankingHelper {
 
 		init();
 
-		const timer: timer = CreateTimer();
-		let count: number = 0;
-		MessageAll(true, `Saving game data`, 0, 0);
-		TimerStart(timer, 1.00, true, () => {
-			try {
-				if (count > 23) {
-					PauseTimer(timer);
-					DestroyTimer(timer);
-					this.endTracking();
-				} else {
-					if (GamePlayer.fromPlayer.has(Player(count)) && !GamePlayer.get(Player(count)).isObserving()) {
-						try {
-							let p: GamePlayer = GamePlayer.get(Player(count));
-							MessageAll(false, `Saving Data for ${p.names.acct}`, 0, 0);
-							setKillsValue(p.player, p.kd.get(p).killValue, "set");
-							setDeathsValue(p.player, p.kd.get(p).deathValue, "set");
-							setKillsTotal(p.player, p.kd.get(p).kills, "set");
-							setDeathsTotal(p.player, p.kd.get(p).deaths, "set");
-							setSSKillValue(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).killValue, "set");
-							setSSDeathValue(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).deathValue, "set");
-							setSSKillTotal(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).kills, "set");
-							setSSDeathTotal(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).deaths, "set");
+		const preTimer: timer = CreateTimer();
+		TimerStart(preTimer, 5.00, false, () => {
+			const timer: timer = CreateTimer();
+			let count: number = 0;
+			MessageAll(true, `Saving game data`, 0, 0);
+			TimerStart(timer, 1.00, true, () => {
+				try {
+					if (count > 23) {
+						PauseTimer(timer);
+						DestroyTimer(timer);
+						this.endTracking();
+					} else {
+						if (GamePlayer.fromPlayer.has(Player(count)) && !GamePlayer.get(Player(count)).isObserving()) {
+							try {
+								let p: GamePlayer = GamePlayer.get(Player(count));
+								MessageAll(false, `Saving Data for ${p.names.acct}`, 0, 0);
+								setKillsValue(p.player, p.kd.get(p).killValue, "set");
+								setDeathsValue(p.player, p.kd.get(p).deathValue, "set");
+								setKillsTotal(p.player, p.kd.get(p).kills, "set");
+								setDeathsTotal(p.player, p.kd.get(p).deaths, "set");
+								setSSKillValue(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).killValue, "set");
+								setSSDeathValue(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).deathValue, "set");
+								setSSKillTotal(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).kills, "set");
+								setSSDeathTotal(p.player, p.kd.get(`${UID.BATTLESHIP_SS}`).deaths, "set");
 
-							let num: number = p.kd.get(p).killValue / p.kd.get(p).deathValue;
-							num = num * 100
-							num = Math.round(num)
-							num = num / 100
-							setKDValue(p.player, num, "set");
+								let num: number = p.kd.get(p).killValue / p.kd.get(p).deathValue;
+								num = num * 100
+								num = Math.round(num)
+								num = num / 100
+								setKDValue(p.player, num, "set");
 
-							num = p.kd.get(p).kills / p.kd.get(p).deaths;
-							num = num * 100
-							num = Math.round(num)
-							num = num / 100
-							setKDTotal(p.player, num, "set");
+								num = p.kd.get(p).kills / p.kd.get(p).deaths;
+								num = num * 100
+								num = Math.round(num)
+								num = num / 100
+								setKDTotal(p.player, num, "set");
 
-							setTurnDied(p.player, p.turnDied, "set");
-							
-							setGoldTotal(p.player, p.goldTotal, "set");
-							setBountyGold(p.player, p.bounty.total, "set");
-							setBonusGold(p.player, p.bonus.total, "set");
+								setTurnDied(p.player, p.turnDied, "set");
 
-							setMaxCities(p.player, p.cityData.maxCities, "set");
-							setEndCities(p.player, p.cityData.endCities, "set");
+								setGoldTotal(p.player, p.goldTotal, "set");
+								setBountyGold(p.player, p.bounty.total, "set");
+								setBonusGold(p.player, p.bonus.total, "set");
 
-							// setTotalRiflemenKills(p.player, p.kd.get(`${UID.RIFLEMEN}`).kills, "set");
-							// setTotalRiflemenDeaths(p.player, p.kd.get(`${UID.RIFLEMEN}`).kills, "set");
-							// setTotalMedicKills(p.player, p.kd.get(`${UID.MEDIC}`).kills, "set");
-							// setTotalMedicDeaths(p.player, p.kd.get(`${UID.MEDIC}`).kills, "set");
-							// setTotalMortarKills(p.player, p.kd.get(`${UID.MORTAR}`).kills, "set");
-							// setTotalMortarDeaths(p.player, p.kd.get(`${UID.MORTAR}`).kills, "set");
-							// setTotalRoarerKills(p.player, p.kd.get(`${UID.ROARER}`).kills, "set");
-							// setTotalRoarerDeaths(p.player, p.kd.get(`${UID.ROARER}`).kills, "set");
-							// setTotalKnightKills(p.player, p.kd.get(`${UID.KNIGHT}`).kills, "set");
-							// setTotalKnightDeaths(p.player, p.kd.get(`${UID.KNIGHT}`).kills, "set");
-							// setTotalGeneralKills(p.player, p.kd.get(`${UID.GENERAL}`).kills, "set");
-							// setTotalGeneralDeaths(p.player, p.kd.get(`${UID.GENERAL}`).kills, "set");
-							// setTotalArtilleryKills(p.player, p.kd.get(`${UID.ARTILLERY}`).kills, "set");
-							// setTotalArtilleryDeaths(p.player, p.kd.get(`${UID.ARTILLERY}`).kills, "set");
-							// setTotalTankKills(p.player, p.kd.get(`${UID.TANK}`).kills, "set");
-							// setTotalTankDeaths(p.player, p.kd.get(`${UID.TANK}`).kills, "set");
-							// setTotalMarineKills(p.player, p.kd.get(`${UID.MARINE}`).kills, "set");
-							// setTotalMarineDeaths(p.player, p.kd.get(`${UID.MARINE}`).kills, "set");
-							// setTotalCaptainKills(p.player, p.kd.get(`${UID.CAPTAIN}`).kills, "set");
-							// setTotalCaptainDeaths(p.player, p.kd.get(`${UID.CAPTAIN}`).kills, "set");
-							// setTotalAdmiralKills(p.player, p.kd.get(`${UID.ADMIRAL}`).kills, "set");
-							// setTotalAdmiralDeaths(p.player, p.kd.get(`${UID.ADMIRAL}`).kills, "set");
-							// setTotalTransportKills(p.player, p.kd.get(`${UID.TRANSPORT_SHIP}`).kills, "set");
-							// setTotalTransportDeaths(p.player, p.kd.get(`${UID.TRANSPORT_SHIP}`).kills, "set");
-							// setTotalHeavyTransportKills(p.player, p.kd.get(`${UID.ARMORED_TRANSPORT_SHIP}`).kills, "set");
-							// setTotalHeavyTransportDeaths(p.player, p.kd.get(`${UID.ARMORED_TRANSPORT_SHIP}`).kills, "set");
-							// setTotalWarshipAKills(p.player, p.kd.get(`${UID.WARSHIP_A}`).kills, "set");
-							// setTotalWarshipADeaths(p.player, p.kd.get(`${UID.WARSHIP_A}`).kills, "set");
-							// setTotalWarshipBKills(p.player, p.kd.get(`${UID.WARSHIP_B}`).kills, "set");
-							// setTotalWarshipBDeaths(p.player, p.kd.get(`${UID.WARSHIP_B}`).kills, "set");
-							setMode(p.player, RoundSettings.mode)
-							if (p.player == who /*|| Alliances.getInstance().isAllied(who, p.player)) && RoundSettings.diplomancy != 3*/) {
-								setPlayerFlag(p.player, "winner");
-							} else {
-								setPlayerFlag(p.player, "loser");
+								setMaxCities(p.player, p.cityData.maxCities, "set");
+								setEndCities(p.player, p.cityData.endCities, "set");
+
+								// setTotalRiflemenKills(p.player, p.kd.get(`${UID.RIFLEMEN}`).kills, "set");
+								// setTotalRiflemenDeaths(p.player, p.kd.get(`${UID.RIFLEMEN}`).kills, "set");
+								// setTotalMedicKills(p.player, p.kd.get(`${UID.MEDIC}`).kills, "set");
+								// setTotalMedicDeaths(p.player, p.kd.get(`${UID.MEDIC}`).kills, "set");
+								// setTotalMortarKills(p.player, p.kd.get(`${UID.MORTAR}`).kills, "set");
+								// setTotalMortarDeaths(p.player, p.kd.get(`${UID.MORTAR}`).kills, "set");
+								// setTotalRoarerKills(p.player, p.kd.get(`${UID.ROARER}`).kills, "set");
+								// setTotalRoarerDeaths(p.player, p.kd.get(`${UID.ROARER}`).kills, "set");
+								// setTotalKnightKills(p.player, p.kd.get(`${UID.KNIGHT}`).kills, "set");
+								// setTotalKnightDeaths(p.player, p.kd.get(`${UID.KNIGHT}`).kills, "set");
+								// setTotalGeneralKills(p.player, p.kd.get(`${UID.GENERAL}`).kills, "set");
+								// setTotalGeneralDeaths(p.player, p.kd.get(`${UID.GENERAL}`).kills, "set");
+								// setTotalArtilleryKills(p.player, p.kd.get(`${UID.ARTILLERY}`).kills, "set");
+								// setTotalArtilleryDeaths(p.player, p.kd.get(`${UID.ARTILLERY}`).kills, "set");
+								// setTotalTankKills(p.player, p.kd.get(`${UID.TANK}`).kills, "set");
+								// setTotalTankDeaths(p.player, p.kd.get(`${UID.TANK}`).kills, "set");
+								// setTotalMarineKills(p.player, p.kd.get(`${UID.MARINE}`).kills, "set");
+								// setTotalMarineDeaths(p.player, p.kd.get(`${UID.MARINE}`).kills, "set");
+								// setTotalCaptainKills(p.player, p.kd.get(`${UID.CAPTAIN}`).kills, "set");
+								// setTotalCaptainDeaths(p.player, p.kd.get(`${UID.CAPTAIN}`).kills, "set");
+								// setTotalAdmiralKills(p.player, p.kd.get(`${UID.ADMIRAL}`).kills, "set");
+								// setTotalAdmiralDeaths(p.player, p.kd.get(`${UID.ADMIRAL}`).kills, "set");
+								// setTotalTransportKills(p.player, p.kd.get(`${UID.TRANSPORT_SHIP}`).kills, "set");
+								// setTotalTransportDeaths(p.player, p.kd.get(`${UID.TRANSPORT_SHIP}`).kills, "set");
+								// setTotalHeavyTransportKills(p.player, p.kd.get(`${UID.ARMORED_TRANSPORT_SHIP}`).kills, "set");
+								// setTotalHeavyTransportDeaths(p.player, p.kd.get(`${UID.ARMORED_TRANSPORT_SHIP}`).kills, "set");
+								// setTotalWarshipAKills(p.player, p.kd.get(`${UID.WARSHIP_A}`).kills, "set");
+								// setTotalWarshipADeaths(p.player, p.kd.get(`${UID.WARSHIP_A}`).kills, "set");
+								// setTotalWarshipBKills(p.player, p.kd.get(`${UID.WARSHIP_B}`).kills, "set");
+								// setTotalWarshipBDeaths(p.player, p.kd.get(`${UID.WARSHIP_B}`).kills, "set");
+								setMode(p.player, RoundSettings.mode)
+								if (p.player == who /*|| Alliances.getInstance().isAllied(who, p.player)) && RoundSettings.diplomancy != 3*/) {
+									setPlayerFlag(p.player, "winner");
+								} else {
+									setPlayerFlag(p.player, "loser");
+								}
+								MessageAll(false, `Data saved for ${p.names.acct}`, 0, 0);
+							} catch (error) {
+								print(`Error for player ${GamePlayer.get(Player(count)).names.acct}\n${error}`)
 							}
-							MessageAll(false, `Data saved for ${p.names.acct}`, 0, 0);
-						} catch (error) {
-							print(`Error for player ${GamePlayer.get(Player(count)).names.acct}\n${error}`)
+
 						}
-
 					}
+				} catch (error) {
+					print(error)
 				}
-			} catch (error) {
-				print(error)
-			}
 
-			count++;
-		});
+				count++;
+			});
 
-		MessageAll(true, `Finished Saving Data`, 0, 0);
-		MessageAll(false, `${HexColors.TANGERINE}This game was ranked|r\n${HexColors.GREEN}wc3stats.com/risk-europe|r\nYou can find rankings at the site above!`, 0, 0);
-		MessageAll(false, `${HexColors.TANGERINE}You can download the official version on the discord!|r\n${HexColors.GREEN}Discord Link:|r discord.me/risk`, 0, 0);
+			MessageAll(true, `Finished Saving Data`, 0, 0);
+			MessageAll(false, `${HexColors.TANGERINE}This game was ranked|r\n${HexColors.GREEN}wc3stats.com/risk-europe|r\nYou can find rankings at the site above!`, 0, 0);
+			MessageAll(false, `${HexColors.TANGERINE}You can download the official version on the discord!|r\n${HexColors.GREEN}Discord Link:|r discord.me/risk`, 0, 0);
+		})
+
+
 	}
 
 	public trackGame() {
