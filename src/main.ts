27fe682e0aa9fe538ddd84c052976c1c3f0408db
chittenden-daniel <danielchittenden@yahoo.com>
2022-10-07 22:15:48 -0700

    remove tracking related codee

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { Game } from "app/game/game-type";
 import { addScriptHook, W3TS_HOOK } from "w3ts";
-import { stopInit } from "w3ts-w3mmd";
 
 function tsMain() {
 	try {
@@ -15,7 +14,6 @@ addScriptHook(W3TS_HOOK.MAIN_AFTER, tsMain);
 
 function fixMapName() {
 	SetMapName("Risk Europe");
-	stopInit();
 }
 
 addScriptHook(W3TS_HOOK.CONFIG_AFTER, fixMapName);
